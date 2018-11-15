5c6734ef8ec92a607440fd1588984048b7f96792
blakestokes <blakejstokes@gmail.com>
2018-11-15 07:29:27 -0700

    start

--- destiny.js
+++ destiny.js
@@ -1,3 +1,40 @@
+var parallaxEnabled = true;
+var mouseX = event.pageX;
+var mouseY = event.pageY;
+var windowWidth = window.innerWidth;
+var	windowHeight = window.innerHeight;
+
+document.querySelector('#lax').onchange = function() {
+    // var checked = document.querySelector('#lax').checked;
+    var checked = this.checked;
+    if (checked == true) {
+        parallaxEnabled = true;
+        document.querySelector('.character').style.marginTop = '350px';
+    }
+    else {
+        parallaxEnabled = false;
+        document.querySelector('.content, .character').style.cssText = "-webkit-transform: translateX(0%) translateY(0%); -moz-transform: translateX(0%) translateY(0%); transform: translateX(0%) translateY(0%)";
+        document.querySelector('.character').style.marginTop = '0px';
+    }
+}
+document.onmousemove = function() {
+    var speed = 30;
+
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+
+    var percentX = ((mouseX/windowWidth) * speed) - (speed/0.75);
+    var percentY = ((mouseY/windowHeight) * speed) - (speed/0.6);
+    var stringX = (0-percentX-speed) + "%";
+    var stringY = (0-percentY-speed) + "%";
+
+    var percentCX = ((mouseX/windowWidth) * speed) - (speed/30);
+    var percentCY = ((mouseY/windowHeight) * speed) - (speed/90);
+    var stringCX = (0-percentCX-speed) + "%";
+    var stringCY = (0-percentCY-speed) + "%";
+}
+
+
 $(document).ready(function(event){
 
     parallaxEnabled = true;
